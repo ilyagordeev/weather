f90fccc4840080f1068893b8bfaf37e4757ba00b
Ilya Gordeev <ilya.tanat@gmail.com>
2019-11-25 10:50:01 +0500

    + bug fixes

--- src/main/java/com/ctest/weather/WeatherUpdater.java
+++ src/main/java/com/ctest/weather/WeatherUpdater.java
@@ -12,7 +12,7 @@ import java.util.Map;
 
 public class WeatherUpdater {
 
-    private Map<String, String> coordinatesCache = new HashMap<>()
+    private static Map<String, String> coordinatesCache = new HashMap<>()
     {{
         put("Екатеринбург", "lat=56.836331&lon=60.605546"); // Ekaterinburg
         put("Москва", "lat=55.748952&lon=37.620076"); // Moscow
@@ -27,17 +27,18 @@ public class WeatherUpdater {
         this.weatherRepository = weatherRepository;
     }
 
-    public void Update(String city, String weatherProvider) {
-        if (weatherProvider.equals("Yandex")) RequestYandex(city);
-        if (weatherProvider.equals("OpenWeather")) RequestOpenWeather(city);
+    public boolean Update(String city, String weatherProvider) {
+        if (weatherProvider.equals("Yandex")) return RequestYandex(city);
+        if (weatherProvider.equals("OpenWeather")) return RequestOpenWeather(city);
+        return false;
     }
 
-    private String getCoordinates(String city) {
+    private String Coordinates(String city) {
         if (!coordinatesCache.containsKey(city)) {
             String coordinates = Geocode(city);
             if (coordinates != null) {
                 coordinatesCache.put(city, coordinates);
-                return coordinatesCache.get(city);
+                return coordinates;
             }
         } else {
             return coordinatesCache.get(city);
@@ -52,28 +53,34 @@ public class WeatherUpdater {
         HttpHeaders headers = new HttpHeaders();
         String body = "";
 
-        HttpEntity<String> requestEntity = new HttpEntity<String>(body, headers);
+        HttpEntity<String> requestEntity = new HttpEntity<>(body, headers);
         ResponseEntity<String> responseEntity = rest.exchange(url,
                 HttpMethod.GET, requestEntity, String.class);
         String response = responseEntity.getBody();
 
         ObjectMapper objectMapper = new ObjectMapper();
         assert response != null;
+
         try {
             JsonNode rootNode = objectMapper.readTree(response);
+            String status = rootNode.path("status").asText();
+            if (status.equals("ZERO_RESULTS")) return null;
             String lat = rootNode.path("results").get(0).path("geometry").path("location").path("lat").asText();
             String lng = rootNode.path("results").get(0).path("geometry").path("location").path("lng").asText();
-            return "lat=" + lat + "&lon=" + lng;
+            String address = rootNode.path("results").get(0).path("formatted_address").asText();
+            System.out.println(address);
+            if (status.equals("OK")) return "lat=" + lat + "&lon=" + lng;
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
         return null;
-
     }
 
-    private void RequestYandex(String city) {
+    private boolean RequestYandex(String city) {
+        String coordinates = Coordinates(city);
+        if (coordinates == null) return false;
         String url = String.format("https://api.weather.yandex.ru/v1/forecast?%s&limit=1&extra=true",
-                                    getCoordinates(city));
+                                    coordinates);
         String body = "";
 
         RestTemplate rest = new RestTemplate();
@@ -106,12 +113,14 @@ public class WeatherUpdater {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+        return true;
     }
 
-    private void RequestOpenWeather(String city) {
+    private boolean RequestOpenWeather(String city) {
+        String coordinates = Coordinates(city);
+        if (coordinates == null) return false;
         String url = String.format("https://api.openweathermap.org/data/2.5/weather?%s&units=metric&APPID=c351d6bef8d8ca2005cd43ddbad73a04",
-                                                        getCoordinates(city));
+                                        coordinates);
         String body = "";
 
         RestTemplate rest = new RestTemplate();
@@ -144,6 +153,7 @@ public class WeatherUpdater {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return true;
 
     }
 
